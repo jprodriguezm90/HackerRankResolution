760e60fbecc9d9a87e5baff8e34cf758cced61ee
Juan <jprodriguezm90@gmail.com>
2026-08-10 12:10:06 -0500

    20260810 - HackerRank exercise view

--- HackerRankResolution/Program.cs
+++ HackerRankResolution/Program.cs
@@ -7,4 +7,32 @@ Console.WriteLine("Here we will call all the submissions of HackerRank");
 
 //ArrayDS.callReverseArray();
 
-_2DArray.callHourglassSum();
+//_2DArray.callHourglassSum();
+
+IEnumerable<int> numbers = Enumerable.Range(1, 5);
+
+var query = numbers.Where(n => n % 2 == 0);
+
+numbers = Enumerable.Range(1, 10);
+
+foreach (var n in query)
+{
+    Console.WriteLine(n);
+}
+Console.WriteLine("------");
+int limit = 5;
+
+var query2 = Enumerable.Range(1, 10)
+    .Where(n => n <= limit);
+
+foreach (var n in query2)
+{
+    Console.WriteLine(n);
+}
+
+limit = 8;
+
+foreach (var n in query2)
+{
+    Console.WriteLine(n);
+}
